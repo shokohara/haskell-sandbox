3906e7e7b4c46bd177cef57069c186ccbe1c714b
shokohara <shokohara@users.noreply.github.com>
2017-02-14 10:23:17 +0900

    .

--- src/LYGHWriter.hs
+++ src/LYGHWriter.hs
@@ -1,6 +1,8 @@
 module LYGHWriter where
 
 import Control.Monad.Writer
+import System.Random
+import Control.Monad.State
 
 logNumber :: Int -> Writer [String] Int
 logNumber x = writer (x, ["Got number: " ++ show x])
@@ -79,4 +81,55 @@ finalCountDown 0 = do
 finalCountDown x = do
   finalCountDown (x-1)
   tell [show x]
+
+
+addStuff :: Int -> Int
+addStuff x = let
+  a = (*2) x
+  b = (+10) x
+  in a+b
+
+threeCoins :: StdGen -> (Bool, Bool, Bool)
+threeCoins gen =
+  let (firstCoin, newGen) = random gen
+      (secondCoin, newGen') = random newGen
+      (thirdCoin, newGen'') = random newGen'
+  in (firstCoin, secondCoin, thirdCoin)
+
+type Stack = [Int]
+
+pop :: State Stack Int
+pop = state $ \(x:xs) -> (x,xs)
+
+push :: Int -> State Stack ()
+push a = state $ \xs -> ((), a:xs)
+
+stackManip :: State Stack Int
+stackManip = do
+  push 3
+  pop
+  pop
+
+stackStuff :: State Stack ()
+stackStuff = do
+  a <- pop
+  if a == 5
+     then push 5
+     else do
+       push 3
+       push 8
+
+moreStack :: State Stack ()
+moreStack = do
+  a <- stackManip
+  if a == 100
+     then stackStuff
+     else return ()
+
+stackyStack :: State Stack ()
+stackyStack = do
+  stackNow <- get
+  if stackNow == [1..3]
+     then put [8,3,1]
+     else put [9,2,1]
 
